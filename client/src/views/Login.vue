a1d092b8f82af769227fa0bc55665453b1427cd0
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-09-19 20:38:08 -0600

    login and register buttons

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="login">
+  <div>
     <form v-if="loginForm" @submit.prevent="loginUser">
       <input type="email" v-model="creds.email" placeholder="email" />
       <input type="password" v-model="creds.password" placeholder="password" />
-      <button class="btn btn-success" type="submit">Login</button>
+      <button class="login-button">Login</button>
     </form>
     <form v-else @submit.prevent="register">
       <input type="text" v-model="newUser.name" placeholder="name" />
       <input type="email" v-model="newUser.email" placeholder="email" />
       <input type="password" v-model="newUser.password" placeholder="password" />
-      <button class="btn btn-warning" type="submit">Create Account</button>
+      <button class="login-button">Create Account</button>
     </form>
-    <div class="action" @click="loginForm = !loginForm">
-      <p v-if="loginForm">No account? Click here to Register</p>
-      <p v-else>Already have an account? Click here to Login</p>
+    <div class="action text-color" @click="loginForm = !loginForm">
+      <p v-if="loginForm">
+        <b>No account? Click here to Register</b>
+      </p>
+      <p v-else>
+        <b>Already have an account? Click here to Login</b>
+      </p>
     </div>
   </div>
 </template>
@@ -55,5 +59,28 @@ export default {
 <style>
 .action {
   cursor: pointer;
+}
+.text-color {
+  color: #908a99;
+}
+.login-button {
+  display: inline-block;
+  padding: 3px 8px;
+  font-size: 15px;
+  text-align: center;
+  outline: none;
+  color: #fff;
+  background-color: #b5b6e4;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 5px #606188;
+}
+.login-button:hover {
+  background-color: #a4a5d3;
+}
+.login-button:active {
+  background-color: #8d8ebe;
+  box-shadow: 0 3px #606188;
+  transform: translateY(4px);
 }
 </style>
